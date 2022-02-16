505c52404ec55264443c404def18251326896363
unknown <abril.hernandez.hernandez.02@gmail.com>
2022-02-16 10:53:20 -0600

    subir spring

---
+++ src/test/java/com/example/SpringExamen/SpringExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
